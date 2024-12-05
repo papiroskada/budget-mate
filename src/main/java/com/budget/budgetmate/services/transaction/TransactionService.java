70f787127eece84e512e6a7086f641838de5f2d2
dashushak <100213341+papiroskada@users.noreply.github.com>
2024-12-05 20:56:21 +0200

    added decorator for validation

--- src/main/java/com/budget/budgetmate/services/transaction/TransactionService.java
+++ src/main/java/com/budget/budgetmate/services/transaction/TransactionService.java
@@ -2,7 +2,6 @@ package com.budget.budgetmate.services.transaction;
 
 import com.budget.budgetmate.dto.TransactionDTO;
 import com.budget.budgetmate.models.Transaction;
-
 public interface TransactionService {
     Transaction postTransaction(TransactionDTO transactionDTO);
 
